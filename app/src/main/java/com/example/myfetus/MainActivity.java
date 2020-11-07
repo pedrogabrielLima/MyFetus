9ff36fd48050c04bef112cbb78d1e82dd107bbcb
franvcg <franvcg@gmail.com>
2020-11-07 17:33:35 -0300

    Ajuste em constraints

--- app/src/main/java/com/example/myfetus/MainActivity.java
+++ app/src/main/java/com/example/myfetus/MainActivity.java
@@ -99,7 +99,7 @@ public class MainActivity extends AppCompatActivity{
                 }
 
 
-        });
+        });//arrombado
         stopbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
